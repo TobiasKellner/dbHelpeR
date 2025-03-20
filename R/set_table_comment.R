6862247e18141e0d2756c25304e7a8b0f1318474
TobiasTDV <tobias.kellner@transdev.de>
2025-03-20 14:45:10 +0100

    UPDATE: improve function

--- R/set_table_comment.R
+++ R/set_table_comment.R
@@ -1,18 +1,21 @@
 #' Create a comment for a database table
 #'
+#' This function sets a comment for the selected database table.
+#'
 #' @param conn A DBIConnection object
-#' @param schema The name of the parent schema of the table
-#' @param table_name The name of the database table
+#' @param schema The name of the parent schema of the database table
+#' @param table The name of the database table
 #' @param comment The comment you want to use for this table
 #'
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples set_table_comment(conn = con, schema = "test_schema", table = "test_table", comment = "This is my test comment.")
 #'
-set_table_comment <- function(conn, schema, table_name, comment) {
+set_table_comment <- function(conn, schema, table, comment) {
+
   # detect database type
-  db_type <- get_db_type(con)
+  db_type <- get_database_type(con)
   db_type <- tolower(db_type)
 
   if (grepl("sql server", db_type)) {
@@ -20,24 +23,25 @@ set_table_comment <- function(conn, schema, table_name, comment) {
   } else if (grepl("postgresql", db_type)) {
     db_type <- "postgres"
   } else {
-    stop("Datenbanktyp wird nicht unterstützt. Unterstützte Typen: MSSQL, PostgreSQL.")
+    stop("Database type is not supported. Supported types are: Microsoft SQL Server, PostgreSQL.")
   }
 
-  # Query basierend auf dem Datenbanktyp erstellen
+  # Create query based on the database type
   query <- switch(
     db_type,
+    "postgres" = paste0(
+      "COMMENT ON TABLE ", schema, ".\"", table, "\" IS '", comment, "';"
+    ),
     "mssql" = paste0(
       "EXEC sp_addextendedproperty \n",
       "  @name = 'MS_Description',\n",
       "  @value = '", comment, "',\n",
       "  @level0type = 'SCHEMA', @level0name = '", schema, "',\n",
-      "  @level1type = 'TABLE', @level1name = '", table_name, "';"
-    ),
-    "postgres" = paste0(
-      "COMMENT ON TABLE ", schema, ".\"", table_name, "\" IS '", comment, "';"
+      "  @level1type = 'TABLE', @level1name = '", table, "';"
     )
   )
 
-  # Query ausführen
+  # execute SQL statement
   dbExecute(con, query)
+
 }
