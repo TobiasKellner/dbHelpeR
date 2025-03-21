82240b2986f75dc756efc0f16bad1aba7c9ab4e0
TobiasTDV <tobias.kellner@transdev.de>
2025-03-21 10:47:53 +0100

    UPDATE: add description and dataframe option

--- R/get_column_type.R
+++ R/get_column_type.R
@@ -1,6 +1,28 @@
-get_column_type <- function(conn, schema = NULL, table_name, column_name) {
-  # Datenbanktyp automatisch erkennen
-  db_type <- get_db_type(con)
+#' Get column type
+#'
+#' This function returns the column type of a selected column respectively a
+#' table of the type assignment of all columns if the 'dataframe' option is set to TRUE.
+#'
+#' @param conn A DBIConnection object.
+#' @param schema The name of the parent schema of the database table.
+#'  If no schema is specified, the default schema of the database is used
+#'  (e.g. 'dbo' for Microsoft SQL Server).
+#' @param table The name of the database table
+#' @param column The name of the column \cr
+#'  Note: If the 'dataframe' is set to TRUE, the 'column' option can be omitted to get a table of all columns with column type mapping
+#' @param dataframe A boolean value for obtaining a character string or dataframe
+#'
+#' @return A character string representing the column type respectively a dataframe object representing the column type if 'dataframe' is set to TRUE
+#' @export
+#'
+#' @examples
+#' get_column_type(conn = con, schema = "schema", table = "table", column = "column", dataframe = FALSE)
+#' get_column_type(conn = con, schema = "schema", table = "table", dataframe = TRUE)
+#'
+get_column_type <- function(conn, schema = NULL, table, column = NULL, dataframe = FALSE) {
+
+  # detect database type
+  db_type <- get_database_type(con)
   db_type <- tolower(db_type)
 
   if (grepl("sql server", db_type)) {
@@ -8,30 +30,38 @@ get_column_type <- function(conn, schema = NULL, table_name, column_name) {
   } else if (grepl("postgresql", db_type)) {
     db_type <- "postgres"
   } else {
-    stop("Datenbanktyp wird nicht unterstützt. Unterstützte Typen: MSSQL, PostgreSQL.")
+    stop("Database type is not supported. Supported types are: Microsoft SQL Server, PostgreSQL.")
   }
 
-  # Handle schema prefix if provided
-  schema_prefix <- if (!is.null(schema)) paste0(schema, ".") else ""
-
-  # Define a switch for supported database types
-  query <- switch(
+  # define a switch for supported database types
+  sql_command <- switch(
     db_type,
     "postgres" = paste0(
-      "SELECT data_type FROM information_schema.columns ",
+      "SELECT column_name AS \"COLUMN_NAME\", data_type AS \"DATA_TYPE\" FROM information_schema.columns ",
       "WHERE table_schema = '", ifelse(is.null(schema), "public", schema), "' ",
-      "AND table_name = '", table_name, "' AND column_name = '", column_name, "';"
+      "AND table_name = '", table,
+      if(!is.null(column)) {paste0("' AND column_name = '", column)},
+      "';"
     ),
     "mssql" = paste0(
-      "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS ",
+      "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS ",
       "WHERE TABLE_SCHEMA = '", ifelse(is.null(schema), "dbo", schema), "' ",
-      "AND TABLE_NAME = '", table_name, "' AND COLUMN_NAME = '", column_name, "';"
+      "AND TABLE_NAME = '", table,
+      if(!is.null(column)) {paste0("' AND COLUMN_NAME = '", column)},
+      "';"
     ),
     stop("Unsupported database type: ", db_type)
   )
 
-  # Query ausführen
-  result <- dbGetQuery(con, query)
+  # execute SQLcommand
+  result <- dbGetQuery(con, sql_command)
 
-  return(result[[1]])
+  if(dataframe == FALSE) {
+    return(result[[1, 2]])
+  } else if(dataframe == TRUE) {
+    return(result)
+  } else {
+    stop("Incorrect input in the 'dataframe' option")
+  }
+
 }
